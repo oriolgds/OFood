4bb323f6418f5e541d534e2f6d924e07ea0d6011
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-06-10 12:26:53 +0000

    Trying with carrousel

--- ofood/src/app/carrousel/carrousel.component.ts
+++ ofood/src/app/carrousel/carrousel.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-carrousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carrousel.component.html',
   styleUrl: './carrousel.component.css'
 })
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class CarrouselComponent implements OnInit {
 
   images: any[] = [];
-  private apiUrl = 'https://foodish-api.com/api/'; // Replace with your API endpoint
+  private apiUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'; // Replace with your API endpoint
 
   constructor() { }
 
@@ -19,19 +19,17 @@ export class CarrouselComponent implements OnInit {
     this.fetchImages();
   }
 
-  async fetchImages(): Promise<void> {
-    for (let i = 0; i < 3; i++) {
-      try {
-        const response = await fetch(this.apiUrl);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const json = await response.json();
-        this.images.push(json.image);
-      } catch (error) {
-        console.error('Error fetching images:', error);
-      }      
-    }  
-    console.log(this.images);
+  async fetchImages(): Promise<any> {
+    try {
+      const response = await fetch(this.apiUrl);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    }      
+    console.log(this.images[0]);
   }
 }

c4180ba5ec8f6b7d48fc56c676962a0a1d9ca11b
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-06-11 07:41:52 +0000

    Search UI added

--- ofood/src/app/app.component.ts
+++ ofood/src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CarrouselComponent } from './carrousel/carrousel.component';
-
+import { SearchBarComponent } from './search-bar/search-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CarrouselComponent],
+  imports: [RouterOutlet, HeaderComponent, CarrouselComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ ofood/src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css'
+})
+export class SearchBarComponent {
+
+}

2e9105d0ed149a5914cb2a22fbd84d2ad86620e6
oriolginger2008 <oriolginger2008@gmail.com>
2024-06-26 20:13:10 +0000

    Finally routing working!

--- ofood/src/app/app.component.ts
+++ ofood/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './header/header.component';
-import { CarrouselComponent } from './carrousel/carrousel.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CarrouselComponent, SearchBarComponent],
+  imports: [RouterOutlet, MainPageComponent, HeaderComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- ofood/src/app/search-bar/search-bar.component.ts
+++ ofood/src/app/search-bar/search-bar.component.ts
@@ -4,6 +4,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-search-bar',
   standalone: true,
@@ -17,9 +19,13 @@ export class SearchBarComponent implements OnInit {
   ngOnInit(): void {
     
   }
-
+  constructor(private router: Router) {}
   Search():void {
-    console.log(this.input)
+    this.router.navigate(['search-results'],  { state: { data: this.input } }).then(nav => {
+      console.log(nav); // true if navigation is successful
+    }, err => {
+      console.log(err) // when there's an error
+    });
   }
   
 }

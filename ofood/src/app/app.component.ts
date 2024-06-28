a48d5975b1aa8d5c317262c1526cb77388ef6be0
oriolginger2008 <oriolginger2008@gmail.com>
2024-06-28 21:19:04 +0000

    Importing bootstrap from node_modules

--- ofood/src/app/app.component.ts
+++ ofood/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { FooterComponent } from './footer/footer.component';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainPageComponent, HeaderComponent, SearchBarComponent, MatSidenavModule],
+  imports: [RouterOutlet, MainPageComponent, HeaderComponent, SearchBarComponent, MatSidenavModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

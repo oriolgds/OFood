efb5b7f0b4c0ffceaf9209a2875f37341b20d365
oriolginger2008 <oriolginger2008@gmail.com>
2024-06-28 21:02:24 +0000

    Sidenav added

--- ofood/src/app/app.component.ts
+++ ofood/src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainPageComponent, HeaderComponent, SearchBarComponent],
+  imports: [RouterOutlet, MainPageComponent, HeaderComponent, SearchBarComponent, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ofood';
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
 }
